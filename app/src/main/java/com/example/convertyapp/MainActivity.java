0c0a2f168b7f3dd4b456faf3bcefd6446c491478
TudorBozu <tudorbozu7@gmail.com>
2022-09-11 22:00:22 +0300

    ConvertApp

--- app/src/main/java/com/example/convertyapp/MainActivity.java
+++ app/src/main/java/com/example/convertyapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.convertyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+public void Convert(View view){
+
+    EditText editText = findViewById(R.id.edittext1);
+    String lei = editText.getText().toString();
+    double leiconvert = Double.parseDouble(lei);
+    double euroconve = leiconvert * 19.44;
+
+    Toast.makeText(this,euroconve+" Lei" , Toast.LENGTH_SHORT).show();
+}
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
